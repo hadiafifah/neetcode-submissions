51f553f4195a3143f46280f6b276c18808105185
hadiafifah <123785665+hadiafifah@users.noreply.github.com>
2026-08-09 13:41:35 -0700

    Add: validate-parentheses - submission-13

---
+++ Data Structures & Algorithms/validate-parentheses/submission-13.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        valid_pairs = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for p in s:
+            if p in valid_pairs:
+                # there must be something in stack AND the last thing is the correct pair
+                if stack and stack[-1] == valid_pairs[p]:
+                    stack.pop()
+                else:
+                    return False
+            # we can assume that the only inputs will be those 6 types of parentheses, so if its not a closing parentheses, it will be an opening one
+            else:
+                stack.append(p)
+        
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+        
+            
